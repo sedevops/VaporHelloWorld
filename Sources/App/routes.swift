c84dfba3bfb04f43371327c0e42c3ece27fa4be2
sedevops <40681393+sedevops@users.noreply.github.com>
2018-12-08 12:07:17 -0800

    added root routing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,6 +5,9 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
+    router.get("") { req in
+        return "Hello from PushTrain"                 
+    }
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world! Test test test"
