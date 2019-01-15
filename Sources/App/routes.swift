61567936b9566f0bd10888a806c71859c06a7f9a
sedevops <40681393+sedevops@users.noreply.github.com>
2019-01-15 17:23:29 +0700

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,11 +6,11 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
     router.get("") { req in
-        return "Hello from PushTrain"                 
+        return "Hello from PushTrain new"                 
     }
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world! Test test test"
+        return "Hello, world! Test test test new"
     }
 
     // Example of configuring a controller
